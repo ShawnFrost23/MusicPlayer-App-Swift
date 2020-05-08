604d483794466b40d0753cb52129f3c218742d72
Arth Patel <arthpatelf50@gmail.com>
2020-05-08 10:59:36 +1000

    Swift Basic Music Player App

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,28 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Arth Patel on 25/4/20.
+//  Copyright © 2020 Arth Patel. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopButtonTapped(_ sender:
+        UIButton) {
+    }
+    @IBAction func nextButtonTapped(_ sender:
+        UIButton) {
+    }
+}
